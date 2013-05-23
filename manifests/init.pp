4cb846710bc02cf88297381af4d758666a300946
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-05-23 16:27:33 +1200

    install the LoadL license and IBMJava packages from a URL

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,7 +27,45 @@
 #     along with the loadleveller Puppet module.  If not, see <http://www.gnu.org/licenses/>.
 
 # [Remember: No empty lines between comments and class definition]
-class loadleveller {
+class loadleveller(
+  $base_url,
+  $ibm_java_pkg = 'IBMJava2-AMD64-142-JRE-1.4.2-5.0',
+  $ibm_java_url = undef,
+  $rhel_dist    = 'RH6-X86_64',
+  $arch         = 'x86_64',
+  $version      = '4.1.1.1-0',
+  $license      = 'full'
+){
 
+  $dep_pkgs = ['wget','libXp','libXScrnSaver']
+
+  package{$dep_pkgs: ensure => installed}
+
+  $rpm_version  = "-${rhel_dist}-${version}"
+  $rpm_tail     = ".${arch}.rpm"
+
+  $licence_pkg  = "LoadL-${license}-license${rpm_version}"
+  $licence_url  = "${base_url}${licence_pkg}${rpm_tail}"
+
+  package{'LoadL_license':
+    name      => $licence_pkg,
+    ensure    => installed.
+    require   => Package['libXP'],
+    provider  => rpm,
+    source    => $licence_url,
+  }
+
+  if $ibm_java_url {
+      $rl_ibm_java_url = $ibm_java_url
+  } else {
+      $rl_ibm_java_url = "${base_url}${ibm_java_pkg}${rpm_tail}"
+  }
+
+  package {'ibm_java':
+    name      => $ibm_java_pkg,
+    ensure    => installed,
+    source    => $rl_ibm_java_url,
+    provider  => rpm
+  }
 
 }
